f45d908bfd7dd251d363762c857a377559a3eaab
Zappah <raz.rimon@overwolf.com>
2025-05-30 18:46:18 +0300

    dotenv

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const EMAILJS_CONFIG = {
-  SERVICE_ID: process.env.EMAILJS_SERVICE_ID,
-  TEMPLATE_ID: process.env.EMAILJS_TEMPLATE_ID,
-  PUBLIC_KEY: process.env.EMAILJS_PUBLIC_KEY
+  SERVICE_ID: process.env.EMAILJS_SERVICE_ID || '',
+  TEMPLATE_ID: process.env.EMAILJS_TEMPLATE_ID || '',
+  PUBLIC_KEY: process.env.EMAILJS_PUBLIC_KEY || ''
 }; 
